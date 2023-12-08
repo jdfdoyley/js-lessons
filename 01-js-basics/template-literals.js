9e874cedd63433dcb8008cf7104536a9409859f4
Jason D'Oyley <jdfd.dev@icloud.com>
2023-12-08 07:17:42 -0500

    Learn about template string

---
+++ 01-js-basics/template-literals.js
@@ -0,0 +1,16 @@
+const firstName = 'Jason';
+const age = 21;
+const profession = 'Software Engineer';
+
+console.log(`I'm ${firstName}, a ${age} year old ${profession}!`);
+
+// Multi-Line String
+console.log(
+  'String with \n\
+multiple \n\
+lines'
+);
+
+console.log(`Another String
+on multiple
+lines.`);
